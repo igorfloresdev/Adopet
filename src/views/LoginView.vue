8210be3130c1eaacd2f6a6a09c9757b479b3ef16
Igor Flores <dev.igorflores@gmail.com>
2022-07-04 20:36:47 -0300

    Initial Commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <PageHeader />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import PageHeader from '@/components/PageHeader.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    PageHeader
+  }
+})
+</script>
